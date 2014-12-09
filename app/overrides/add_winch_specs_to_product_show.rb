498e8dde0e4f3bd52c0dbf7e93404cc03689fa15
Lisa <lisa@pogodan.com>
2014-12-09 04:12:31 +0100

    product/show override should reference partial

--- app/overrides/add_winch_specs_to_product_show.rb
+++ app/overrides/add_winch_specs_to_product_show.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path:  "spree/products/_cart_form",
                      name:          "add_winch_specs_to_product_show",
-                     insert_bottom: "[data-hook='product_price']",
-                     text:          "spree/products/extra_variant_specs"
+                     insert_after: "[data-hook='product_price']",
+                     partial:       "spree/products/extra_variant_specs",
                      )
