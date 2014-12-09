a0066edbcd1d14a1fc6ffbea8c660fbf7dcaa685
Lisa <lisa@pogodan.com>
2014-12-09 04:32:43 +0100

    add all new variants to variants/_form

--- app/overrides/add_winch_specs_to_product_edit.rb
+++ app/overrides/add_winch_specs_to_product_edit.rb
@@ -1,8 +1,5 @@
 Deface::Override.new(virtual_path:  "spree/admin/variants/_form",
                      name:          "add_winch_specs_to_product_edit",
                      insert_bottom: "[data-hook='admin_variant_form_additional_fields']",
-                     text:          "
-                                     <%= f.label :rope_diameter, Spree.t(:rope_diameter) %>
-                                     <%= f.text_field :rope_diameter, :class => 'form-control' %>
-                                    "
-                      )  
+                     partial:       "spree/admin/variants/extra_variant_specs_in_edit"
+                    )  
